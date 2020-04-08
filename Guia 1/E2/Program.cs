081ae01d2baec1b8b238d203cd1f8e85d9a8251a
gomezdavid <davidalejandrogomez2002@gmail.com>
2020-04-08 18:14:20 -0300

    Falta agregar detalle a division

--- Guia 1/E2/Program.cs
+++ Guia 1/E2/Program.cs
@@ -6,7 +6,34 @@ namespace E2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 0, b = 0, multiplicacion=0,resto=0,cont=0;
+
+            Console.WriteLine("Ingrese dos numeros:");
+            
+            a=Int32.Parse(Console.ReadLine());
+            
+            b=Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < b; i++)
+            {
+                multiplicacion=multiplicacion+a;
+            }
+
+            Console.WriteLine("\nEl resultado de la multiplicacion es: " + multiplicacion);
+
+            while( a >= 0)
+            {
+                a=a-b;
+                if(a>=0)
+                {
+                  cont+=1;  
+                }else{
+                    resto=a+b;
+                } 
+            }
+
+            Console.WriteLine("El cociente es: " + cont);
+            Console.WriteLine("El resto es: " + resto);
         }
     }
 }
