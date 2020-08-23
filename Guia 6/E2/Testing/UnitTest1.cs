5518e4e0ec1ed1d2917e7ba171979a1853efdbcf
gomezdavid <davidalejandrogomez2002@gmail.com>
2020-08-23 15:42:49 -0300

    Testing

--- Guia 6/E2/Testing/UnitTest1.cs
+++ Guia 6/E2/Testing/UnitTest1.cs
@@ -1,18 +1,29 @@
 using NUnit.Framework;
+using Ejercicio;
+using System.Collections.Generic;
 
 namespace Testing
 {
     public class Tests
     {
+        
+        private Mago HarryPostre;
+        private Receta receta;
+        //private Ingrediente ingrediente;
+
         [SetUp]
         public void Setup()
         {
+            HarryPostre = new Mago(new VaritaDeCarpe(0),new List<Receta>());
+            receta = new Receta(new List<Ingrediente>(),new List<int>());
+            HarryPostre.ListaDeRecetas.Add(new Receta(new List<Ingrediente>{new Ingrediente(4000,"Ingrediente 1"),new Ingrediente(400,"Ingrediente 2"),new Ingrediente(400,"Ingrediente 3")},new List<int>{3}));
+            HarryPostre.ListaDeRecetas.Add(new Receta(new List<Ingrediente>{new Ingrediente(400,"Ingrediente 1"),new Ingrediente(400,"Ingrediente 2"),new Ingrediente(400,"Ingrediente 3")},new List<int>{3}));
         }
 
         [Test]
-        public void Test1()
+        public void TestParaSaberSiTodasLasRecetasSonDeGordo()
         {
-            Assert.Pass();
+            Assert.AreEqual(true,receta.EsDeGordo());
         }
     }
 }
